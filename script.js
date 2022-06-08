4692674de1716bcd9e887c4f6971f41b49fe0acf
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-07 23:23:13 -0700

    local storage

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
     })
 
-    function timeTracker() {
+    function clockTrack() {
         var timeNow = moment().hour();
 
         // timeblock loop
@@ -39,8 +39,17 @@ $(document).ready(function() {
         })
     }
 
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    
-}
-
-)
+    clockTrack();
+})
