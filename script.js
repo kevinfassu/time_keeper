3dfbc4d221d16c9fb62f3ccb9e81b1779ba61758
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-07 22:25:53 -0700

    add a svebtn listener and save to local storage

--- script.js
+++ script.js
@@ -2,4 +2,15 @@
 var dateToday = moment().format('dddd, MMM Do YYYY, h:mm:ss a')
 $("#currentDay").html(dateToday);
 
-$(document).ready(function())
+$(document).ready(function() {
+    //savebtn listener
+    $(".saveBtn").on("click", function (){
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //save local storage
+        localStorage.setItem(text, time);
+    })
+}
+
+)
