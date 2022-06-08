456db68455b5c3b680f1f22a2e8f365f51c9f68d
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-07 22:07:40 -0700

    added day,date, and time

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
+// This is to show the cureent day, date, and time
+var dateToday = moment().format('dddd, MMM Do YYYY, h:mm:ss a')
+$("#currentDay").html(dateToday);
 
-
+$(document).ready(function())
