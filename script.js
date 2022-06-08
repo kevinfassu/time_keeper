fb16cf85136c549614340c5cf400ce4c3b0b9b6c
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-07 22:29:32 -0700

    attempted to do timeblock loop

--- script.js
+++ script.js
@@ -10,7 +10,37 @@ $(document).ready(function() {
 
         //save local storage
         localStorage.setItem(text, time);
+
     })
+
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        // timeblock loop
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+
+    
 }
 
 )
